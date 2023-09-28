6f88487fca825fc4b9a0e64992b8577825d6de92
Stefan Droste <stefan.droste@posteo.de>
2023-09-28 05:43:46 +0200

    DA finale

--- script/script.js
+++ script/script.js
@@ -1,18 +1,82 @@
-// @ts-check
 
 const contentContainer = document.getElementById('content');
+let currentPlayer = false;
 
 let fields = [
-    'circle', // field 0
+    null, // field 0
     null, // field 1
-    'circle', // field 2
+    null, // field 2
     null, // field 3
     null, // field 4
     null, // field 5
-    'cross', // field 6
-    'cross', // field 7
+    null, // field 6
+    null, // field 7
     null, // field 8
 ];
+
+function restartGame() {
+    fields = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+    ];
+    render();
+}
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6], // diagonal
+];
+
+function isGameFinished() {
+    return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+function getWinningCombination() {
+    for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+        const [a, b, c] = WINNING_COMBINATIONS[i]; // [0, 1, 2]
+        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+            return WINNING_COMBINATIONS[i];
+        }
+    }
+    return null;
+}
+
+function drawWinningLine(combination) {
+    const lineColor = '#ffffff';
+    const lineWidth = 5;
+
+    const startCell = document.querySelectorAll(`td`)[combination[0]];
+    const endCell = document.querySelectorAll(`td`)[combination[2]];
+    const startRect = startCell.getBoundingClientRect();
+    const endRect = endCell.getBoundingClientRect();
+
+    const contentRect = document.getElementById('content').getBoundingClientRect();
+
+    const lineLength = Math.sqrt(
+        Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+    );
+    const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+    const line = document.createElement('div');
+    line.style.position = 'absolute';
+    line.style.width = `${lineLength}px`;
+    line.style.height = `${lineWidth}px`;
+    line.style.backgroundColor = lineColor;
+    line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2 - contentRect.top}px`;
+    line.style.left = `${startRect.left + startRect.width / 2 - contentRect.left}px`;
+    line.style.transform = `rotate(${lineAngle}rad)`;
+    line.style.transformOrigin = `top left`;
+    line.style.transform = `rotate(${lineAngle}rad)`;
+    document.getElementById('content').appendChild(line);
+}
 
 function render() {
     const contentDiv = document.getElementById('content');
@@ -29,14 +93,17 @@ function render() {
             } else if (fields[index] === 'cross') {
                 symbol = generateCrossSVG();
             }
-            tableHtml += `<td>${symbol}</td>`;
+            tableHtml += `<td onclick="setStone(${index})" id="field${index}">${symbol}</td>`;
         }
         tableHtml += '</tr>';
     }
     tableHtml += '</table>';
 
     // Set table HTML to contentDiv
-    contentDiv.innerHTML = tableHtml;
+    if (contentDiv) {
+        contentDiv.innerHTML = tableHtml;
+    }           
+
 }
 
 function init() {
@@ -50,7 +117,7 @@ function generateCircleSVG() {
     const fillColor = '#00B0EF';
     return `<svg width="${width}" height="${height}">
               <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
-                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.5s" fill="freeze" />
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.2s" fill="freeze" />
               </circle>
             </svg>`;
 }
@@ -64,13 +131,13 @@ function generateCrossSVG() {
       <svg width="${width}" height="${height}">
         <line x1="0" y1="0" x2="${width}" y2="${height}"
           stroke="${color}" stroke-width="10">
-          <animate attributeName="x2" values="0; ${width}" dur="500ms" />
-          <animate attributeName="y2" values="0; ${height}" dur="500ms" />
+          <animate attributeName="x2" values="0; ${width}" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
         </line>
         <line x1="${width}" y1="0" x2="0" y2="${height}"
           stroke="${color}" stroke-width="10">
-          <animate attributeName="x2" values="${width}; 0" dur="500ms" />
-          <animate attributeName="y2" values="0; ${height}" dur="500ms" />
+          <animate attributeName="x2" values="${width}; 0" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
         </line>
       </svg>
     `;
@@ -78,6 +145,18 @@ function generateCrossSVG() {
     return svgHtml;
 }
 
+function setStone(index) {
+    const currentFiled = document.getElementById(`field${index}`);
+    currentPlayer = !currentPlayer;
+    fields[index] = currentPlayer ? 'cross' : 'circle';
+    currentFiled.innerHTML = currentPlayer ? generateCrossSVG() : generateCircleSVG();
+    document.getElementById(`field${index}`).onclick = '';
+
+    if (isGameFinished()) {
+        const winCombination = getWinningCombination();
+        drawWinningLine(winCombination);
+    }
+}
 
 
 
